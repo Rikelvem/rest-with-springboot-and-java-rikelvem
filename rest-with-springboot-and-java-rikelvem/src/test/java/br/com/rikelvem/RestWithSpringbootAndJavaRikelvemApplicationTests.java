cf54ab859e5bf228c751c07d3a6924de62ce1ae2
Rikelvem <rikelvemrsilva@gmail.com>
2025-06-09 16:49:45 -0300

    � � :tada: commit inicial

---
+++ rest-with-springboot-and-java-rikelvem/src/test/java/br/com/rikelvem/RestWithSpringbootAndJavaRikelvemApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rikelvem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWithSpringbootAndJavaRikelvemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
